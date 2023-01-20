09b259521af84949abdc3d6084fd3b9474c868e8
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2023-01-20 20:54:24 +0530

    Add files via upload

---
+++ BusinessLayer/Interface/IOrderBusiness.cs
@@ -0,0 +1,13 @@
+﻿using CommonLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interface
+{
+    public interface IOrderBusiness
+    {
+        public OrderModel AddOrder(int UserId, OrderModel orderModel);
+        public List<OrderModel> GetAllOrders(int UserId);
+    }
+}
