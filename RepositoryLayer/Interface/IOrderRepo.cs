3685d842589b89cdadf42f001460bdf702947ee9
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2023-01-20 20:53:42 +0530

    Add files via upload

---
+++ RepositoryLayer/Interface/IOrderRepo.cs
@@ -0,0 +1,13 @@
+﻿using CommonLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IOrderRepo
+    {
+        public OrderModel AddOrder(int UserId, OrderModel orderModel);
+        public List<OrderModel> GetAllOrders(int UserId);
+    }
+}
